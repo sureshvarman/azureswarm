fb0c959da58df988244bb982b51fa3099fe7f495
gr4per <arne.graeper@opuscapita.com>
2017-03-20 18:00:01 +0100

    fixing #1

--- master-install.sh
+++ master-install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 DATACENTER=$1
+MASTERVMNAME=$2
 echo "datacenter=$1"
+echo "masterVmName=$2"
 echo "updating apt" && sudo apt-get update
 echo "loading key from p80.pool.sks-keyservers.net"
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
@@ -12,9 +14,9 @@ echo "adding user to docker group" && sudo usermod -aG docker dm
 sudo echo "setting nodeType in /etc/docker/daemon.json" && echo '{  "labels": ["nodetype=master"]}' > daemon.json
 sudo mv daemon.json /etc/docker/
 sudo echo "restarting docker" && systemctl restart docker
-if [ "init" = "$2" ]
+if [ "init" = "$3" ]
   then sudo echo "initializing swarm" && sudo docker swarm init --advertise-addr eth0
-  else echo "joining swarm" && sudo docker swarm join --token $(nc swarmMaster0 8888 | tail -n1) swarmMaster0:2377
+  else JOINTOKEN=$(curl $2:8888/manager-token.txt); echo "joining swarm with token $JOINTOKEN" && sudo docker swarm join --token $JOINTOKEN $2:2377
 fi
 sudo echo "adding consul agent config"
 ADV_ADDR=$(ifconfig | grep -A1 "eth0" | grep -o "inet addr:\S*" | grep -o -e "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*")
@@ -26,10 +28,14 @@ sudo mkdir /consul/config
 sudo mkdir /consul/data
 #sudo echo "{\"node_name\": \"$DOCKER_HOST\",\"advertise_addr\": \"$ADV_ADDR\"}" > /consul/agent-config.json
 sudo echo "{\"datacenter\": \"$DATACENTER\",\"advertise_addr\": \"$ADV_ADDR\"}" > /consul/config/agent-config.json
-sudo echo "configuring netcat swarm token publish via rc.local"
+sudo echo "configuring swarm token publish via rc.local"
 sudo systemctl enable rc-local.service
 sudo echo '#!/bin/bash
-while true ; do sudo docker swarm join-token worker | grep -o "SWMTKN\S*" > tokens.txt; sudo docker swarm join-token manager | grep -o "SWMTKN\S*" >> tokens.txt; cat tokens.txt | nc -l -p 8888; done' > publish_token.sh
+mkdir /swarmtokenshare
+sudo docker swarm join-token worker | grep -o "SWMTKN\S*" > /swarmtokenshare/worker-token.txt
+sudo docker swarm join-token manager | grep -o "SWMTKN\S*" >> /swarmtokenshare/manager-token.txt
+cd /swarmtokenshare
+python -m SimpleHTTPServer 8888' > publish_token.sh
 sudo chmod +x publish_token.sh
 sudo cp publish_token.sh /etc/rc.local
 sudo echo "publishing swarm token"
