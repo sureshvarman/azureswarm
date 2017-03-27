ec8a7881556d9ceee0e8fe5c4e12c1a90cfeb9d5
gr4per <arne.graeper@opuscapita.com>
2017-03-27 12:55:58 +0200

    fixing consul mode

--- worker-install.sh
+++ worker-install.sh
@@ -38,7 +38,7 @@ sudo echo "{\"datacenter\": \"$DATACENTER\",\"advertise_addr\": \"$ADV_ADDR\"}" 
 HOSTPREFIX=${MASTERVMNAME%?}
 NAMESERVER=$(cat /etc/resolv.conf | grep "nameserver" | tail -n1 | grep -o "\S*" | grep -v "nameserver")
 echo "using nameserver $NAMESERVER found in /etc/resolv.conf as consul recursor"
-sudo docker run -d -v /consul/data:/consul/data -v /consul/config:/consul/config --restart always --env SERVICE_IGNORE=true -p 8300:8300 -p 8301:8301 -p 8301:8301/udp -p 8302:8302 -p 8302:8302/udp -p 8400:8400 -p 8500:8500 -p 53:8600/udp consul agent -server -join ${HOSTPREFIX}0 -join ${HOSTPREFIX}1 -join ${HOSTPREFIX}2 -bootstrap-expect 3 -ui -data-dir=/consul/data -config-dir=/consul/config -client 0.0.0.0 -node $DOCKER_HOST -recursor $NAMESERVER
+sudo docker run -d -v /consul/data:/consul/data -v /consul/config:/consul/config --restart always --env SERVICE_IGNORE=true -p 8300:8300 -p 8301:8301 -p 8301:8301/udp -p 8302:8302 -p 8302:8302/udp -p 8400:8400 -p 8500:8500 -p 53:8600/udp consul agent -join ${HOSTPREFIX}0 -join ${HOSTPREFIX}1 -join ${HOSTPREFIX}2 -ui -data-dir=/consul/data -config-dir=/consul/config -client 0.0.0.0 -node $DOCKER_HOST -recursor $NAMESERVER
 sudo echo "starting consul registrator"
 sudo docker run -d --net=host --restart always --volume=/var/run/docker.sock:/tmp/docker.sock gliderlabs/registrator -ip $ADV_ADDR consul://localhost:8500
 sudo echo "configuring name resolution to include consul"
